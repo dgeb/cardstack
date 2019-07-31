b689512f85768a0f453913ff8031788efff4561e
Jen Weber <j@jenweber.me>
2019-07-31 17:22:16 -0400

    use isogit to create a Remote #567

--- packages/git/remote.js
+++ packages/git/remote.js
@@ -4,8 +4,9 @@ git.plugins.set('fs', fs);
 const  { Remote } = require('nodegit');
 
 class IsoRemote {
-    static create(repo, name, url) {
-        return Remote.create(repo, name, url);
+    static async create(repo, name, url) {
+        await git.addRemote({fs, remote: name, url, gitdir: repo.path()});
+        return Remote.lookup(repo, name);
     }
 }
 
